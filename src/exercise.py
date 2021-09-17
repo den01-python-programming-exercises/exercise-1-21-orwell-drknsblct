e01fb84474c5d4c970ae95800aacf5dc15ceabd9
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:11:26 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    num = int(input('Give a number:'))
+    
+    if num == 1984:
+        print('Orwell')
 
 if __name__ == '__main__':
     main()
